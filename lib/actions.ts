82c6e7f17280a5b0d9f3bb31157b359841fbd065
moathssnck <myneww00@gmail.com>
2025-09-22 16:47:58 +0300

    Update actions.ts

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,7 +1,7 @@
 
 
 export const playNotificationSound = () => {
-    const audio=new Audio('/bub.mp3')
+    const audio=new Audio('/ringtone-you-would-be-glad-to-know.mp3')
     if (audio) {
       audio!.play().catch((error) => {
         console.error('Failed to play sound:', error);
